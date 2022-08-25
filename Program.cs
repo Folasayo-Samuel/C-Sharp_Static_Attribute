7cbdfbb0c0d9d4a6ea950a5112d256804dd183a1
Folasayo Samuel <folasayosamuel@gmail.com>
2022-08-25 21:35:14 +0100

    update

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApplication
+{
+	class Songs
+	{
+		static void Main(string[]args)
+		{
+			Song holiday = new Song("Holiday", "Green Day", 200);
+		Song kashmir = new Song("Kashmir", "Led Zeppelin", 150);
+		
+		Console.WriteLine(holiday.title);
+		Console.WriteLine(holiday.artist);
+		
+		Console.ReadLine();
+		}
+	}
+}
---
+++ Song.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApplication
+{
+	class Song
+	{
+		public string title;
+		public string artist;
+		public int duration;
+		
+		public Song(string aTitle, string aArtist, int aDuration)
+		{
+			title = aTitle;
+			artist = aArtist;
+			duration = aDuration;
+		}
+	}
+}
